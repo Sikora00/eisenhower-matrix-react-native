a5001cebd281d7a3083764773fc47442558a20ca
Maciej Sikorski <maciejsikorski00@gmail.com>
2017-10-24 22:50:05 +0200

    Launched Base View

--- App.js
+++ App.js
@@ -9,49 +9,179 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+    TextInput,
+  ScrollView
 } from 'react-native';
+// import TaskComponent from './app/components/Task';
+// import ApiUtils from './app/components/ApiUtils'
+// import Task from './app/entities/Task.js'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+
+    state = {
+        taskArray: [],
+        taskText: '',
+    };
+    constructor() {
+        super();
+        this.pullTasks();
+    }
+
+    render() {
+
+        let tasks = this.state.taskArray.map((task, key) => {
+            // return <TaskComponent key={key} keyval={key} task={task} deleteMethod={() => this.deleteTask(key)}/>
+        });
+
+        return (
+            <View style={styles.container}>
+              <View style={styles.header}>
+                <Text style={styles.headerText}>= Eisenhower Matrix -</Text>
+              </View>
+
+              <ScrollView style={styles.scrollContainer}>
+                  {/*{tasks}*/}
+              </ScrollView>
+
+              <View style={styles.footer}/>
+              <TouchableOpacity onPress={this.addTask.bind(this)} style={styles.addButton}>
+                <Text style={styles.addButtonText}>+</Text>
+              </TouchableOpacity>
+
+              <TextInput style={styles.textInput}
+                         onChangeText={(taskText) => this.setState({taskText})} value={this.state.taskText}
+                         placeholder='> task' placeholderTextColor='white' underlineColorAndroid='transparent'>
+              </TextInput>
+
+            </View>
+
+        );
+    }
+
+    addTask() {
+        if (this.state.taskText) {
+            fetch('http://127.0.0.1:8000/' + 'task', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        title: this.state.taskText
+                    })
+                }
+            )
+                .then(ApiUtils.checkStatus)
+                .then(response => response.json())
+                .then((task) => {
+                    this.state.taskArray.push(
+                        new Task(task.id, task.title)
+                    );
+                    this.setState({taskArray: this.state.taskArray});
+                })
+                .catch(e => e)
+                .done();
+
+            this.setState({taskText: ''});
+
+        }
+    }
+
+    deleteTask(key) {
+        task = this.state.taskArray[key];
+        fetch('http://127.0.0.1:8000/' + 'task/'+task.id, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            }
+        )
+            .then(ApiUtils.checkStatus)
+            .then(response => response.json())
+            .catch(e => e)
+            .done();
+
+        this.state.taskArray.splice(key, 1);
+        this.setState({taskArray: this.state.taskArray});
+    }
+
+    pullTasks() {
+        this.state.taskArray = [];
+        fetch('http://127.0.0.1:8000/' + 'task', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((res) => {
+                res.forEach((task) => {
+                    this.state.taskArray.push(
+                        new Task(task.id, task.title)
+                    );
+                    this.setState({taskArray: this.state.taskArray});
+                });
+            })
+            .catch(e => e)
+            .done();
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#00b1e9',
+        alignItems: 'center',
+        borderBottomWidth: 10,
+        borderBottomColor: '#ddd',
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 18,
+        padding: 26,
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 0,
+    },
+    footer: {
+        position: 'absolute',
+        alignItems: 'center',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    addButton: {
+        backgroundColor: '#0b66e9',
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+        borderColor: '#ccc',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8,
+        marginBottom: -45,
+        zIndex: 10,
+    },
+    addButtonText: {
+        color: '#fff',
+        fontSize: 24,
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth: 10,
+        borderTopColor: '#ededed',
+        paddingTop: 60,
+    }
 });
---
+++ app/entities/Task.js
@@ -0,0 +1,19 @@
+export default class myClass {
+    id;
+    title;
+    constructor(id, title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    getId()
+    {
+        return this.id
+    }
+
+    getTitle()
+    {
+        return this.title;
+    }
+
+}
